ae0c7f9584ae284f20e3fc36d83f19a7442ae18f
Gabriel Matheus Lopes Zambuzi <gabriel.zambuzi@ibm.com>
2023-04-13 09:31:25 -0300

    feat: position class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class App {
   public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
+
     System.out.println("Hello, World!");
+    Position position = new Position(3, 5);
+    System.out.println(position);
+
     scanner.close();
   }
 }
